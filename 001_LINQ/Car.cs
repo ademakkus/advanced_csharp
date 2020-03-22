f45ec73101d40ad0702fe12021b163fe1bc9f136
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2020-03-22 19:23:25 +0300

    Add files via upload

---
+++ 001_LINQ/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_LINQ
+{
+	class Car
+	{
+		public string PetName { get; set; }
+		public string Color { get; set; }
+		public int Speed { get; set; }
+		public string Make { get; set; }
+
+	}
+}
---
+++ 001_LINQ/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.ConsoleColor;
+
+namespace _001_LINQ
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Car> myCars = new List<Car>()
+			{
+
+
+				new Car{ PetName = "Henry", Color = "Silver", Speed = 100, Make = "BMW"},
+			new Car{ PetName = "Daisy", Color = "Tan", Speed = 90, Make = "BMW"},
+new Car{ PetName = "Mary", Color = "Black", Speed = 55, Make = "VW"},
+new Car{ PetName = "Clunker", Color = "Rust", Speed = 5, Make = "Yugo"},
+new Car{ PetName = "Melvin", Color = "White", Speed = 43, Make = "Ford"}
+		};
+			GetFastCars(myCars);
+			GetFastBMWs(myCars);
+			ReadKey();
+
+		}
+		static void	GetFastCars(List<Car> myCars)
+		{
+
+			var fastCars = from c in myCars where c.Speed > 55 select c;
+			foreach (var car in fastCars)
+			{
+				WriteLine("{0} is going too fast !", car.PetName); 
+				//WriteLine($"{car.PetName} is going too fast !"); //with c#6
+
+			}
+		}
+		static void GetFastBMWs(List<Car> myCars)
+		{
+
+			var fastCars = from c in myCars where c.Speed > 55 && c.Make=="Bmw".ToUpper() select c;
+			foreach (var car in fastCars)
+			{
+				WriteLine("{0} is going too fast !", car.PetName);
+				//WriteLine($"{car.PetName} is going too fast !"); //with c#6
+
+			}
+		}
+	}
+}
