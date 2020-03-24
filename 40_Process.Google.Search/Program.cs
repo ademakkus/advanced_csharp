dc529602d98e994aabda1166e89f4802a83531b5
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2020-03-24 14:38:18 +0300

    Add files via upload

---
+++ 40_Process.Google.Search/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using static System.Console;
+using static System.ConsoleColor;
+namespace _40_Process.Google.Search
+{
+	class Program
+	{
+		
+		static void Main(string[] args)
+		{
+
+			SearchGoogle("adem akkuş");
+			ReadKey();
+		}
+		static void SearchGoogle(string term)
+		{
+			Process.Start("http://google.com/search?q=" + term);
+		}
+	}
+}
---
+++ 41_Thread.ForegroundThread/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using static System.Console;
+using static System.ConsoleColor;
+namespace _41_Thread.ForegroundThread
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			Thread thread = new Thread(MyForegroundThread);
+			
+			thread.Start();
+			ForegroundColor = DarkYellow;
+			WriteLine("Main() thread ends..");
+			ReadKey();
+		}
+		static void MyForegroundThread()
+		{
+			ForegroundColor = DarkGreen;
+			for (int i = 0; i != 10; i++)
+			//for (int i = 0; i != 10; i++)
+			{
+				WriteLine(i+"MyForegroundThread() is in progress...");
+				Thread.Sleep(500);
+
+			}
+			ForegroundColor = DarkRed;
+			WriteLine("MyForegroundThread() ends...");
+		
+		}
+	}
+}
