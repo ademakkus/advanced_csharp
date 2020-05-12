735ae53ce9fe3654a0d0f0167646ca60c94eb993
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2020-05-12 19:31:45 +0300

    Add files via upload

---
+++ BinaryWriter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using static System.Console;
+
+
+namespace İleriCsharp.BinaryWriterProject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BinaryWriter binaryWriter = null;
+
+			try
+			{
+				binaryWriter = new BinaryWriter(new FileStream(@"C:\Users\mypc\source\repos\KocaAkademi.İleriCsharp\BinaryWriter\data", FileMode.Open));
+				binaryWriter.Write("BinaryWriter ile yazılan data \n");
+				binaryWriter.Write(DateTime.Now.ToString());
+			}
+			catch (Exception exc)
+			{
+
+				throw exc;
+			}
+
+			finally
+			{
+				if (binaryWriter!=null)
+				{
+					binaryWriter.Close();
+					binaryWriter.Dispose();
+				}
+			}
+			Console.ReadKey();
+			
+		}
+	}
+}
---
+++ İleriCsharp.BinaryeaderProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+namespace İleriCsharp.BinaryeaderProject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BinaryReader binaryReader;
+			try
+			{
+				binaryReader = new BinaryReader(new FileStream(@"C:\Users\mypc\source\repos\KocaAkademi.İleriCsharp\İleriCsharp.BinaryeaderProject\data", FileMode.Open));
+				WriteLine(binaryReader.ReadString());
+			
+			}
+			catch (Exception exc)
+			{
+
+				throw exc;
+			}
+		
+			
+
+
+			ReadKey();
+		}
+	}
+}
